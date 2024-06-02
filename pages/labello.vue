e7eb1d2b998da9f617c4b318dd245fcee0471e66
albertoz <28191614+zabealbe@users.noreply.github.com>
2024-06-03 00:48:35 +0200

    feat: label editing

--- pages/labello.vue
+++ pages/labello.vue
@@ -6,11 +6,6 @@ import { showDirectoryPicker, showOpenFilePicker } from 'file-system-access'
 import JSZip from 'jszip'
 import paper from 'paper'
 
-const shapeLengths = {
-    newBoundingBox: 2,
-    newRotatedBoundingBox: 3,
-    newPolygon: 3,
-}
 const allowedImageExtensions = ['jpg', 'jpeg', 'png']
 
 const state = ref('idle')
@@ -27,9 +22,176 @@ const tool = ref(null)
 var files = []
 var labels = {}
 
+class Label extends paper.Group {
+    constructor(children) {
+        super([children])
+
+        for (let i = 0; i < children.segments.length; i++) {
+            const corner = new paper.Path.Circle({
+                center: children.segments[i].point,
+                radius: paper.settings.handleSize * 2,
+                visible: true,
+                fillColor: new paper.Color(0, 0, 0, 0.00001),
+                insert: false,
+            })
+
+            corner.onMouseEnter = () => this.focus()
+            corner.onMouseLeave = () => this.unfocus()
+            corner.onMouseDrag = (e) => {
+                this.updateCorner(i, e)
+            }
+
+
+            this.children.push(corner)
+        }
+    }
+
+    updateHandles() {
+        for (let i = 0; i < this.children[0].segments.length; i++) {
+            this.children[i + 1].position = this.children[0].segments[i].point
+        }
+    }
+
+    focus() {
+        this.children[0].selected = true
+    }
+    unfocus() {
+        this.children[0].selected = false
+    }
+    scale(scale, origin) {
+        super.scale(scale, origin)
+        this.children.slice(1).forEach(corner => {
+            corner.scale(1 / scale, corner.position)
+        })
+    }
+    remove() {
+        this.children.forEach(corner => {
+            corner.remove()
+        })
+        super.remove()
+    }
+}
+
+class LabelRectangle extends Label {
+    static min_points = 2
+    static num_points = 2
+    constructor(points) {
+        super(new paper.Path.Rectangle({
+            from: points[0],
+            to: points[1],
+            strokeColor: "#00ff00",
+        }))
+    }
+
+    updateCorner(i, e) {
+        const shape = this.children[0]
+
+        const corner = e.point
+        const opposite = shape.segments[(i + 2) % 4]
+
+        shape.segments[i].point = corner
+        shape.segments[(i + 1) % 4].point = new paper.Point(corner.x, opposite.point.y)
+        shape.segments[(i + 3) % 4].point = new paper.Point(opposite.point.x, corner.y)
+
+        this.updateHandles()
+    }
+
+    get points() {
+        return [this.children[0].segments[0].point, this.children[0].segments[2].point]
+    }
+}
+
+class LabelRotatedRectangle extends Label {
+    static min_points = 2
+    static num_points = 3
+    constructor(points) {
+        if (points.length === 2) {
+            points.push(points[1].clone())
+        }
+
+        let s = points[1].subtract(points[0]).normalize();
+        let t = new paper.Point(-s.y, s.x).normalize();
+
+        let h = 0
+        if (points.length === 3) {
+            h = points[2].subtract(points[1]).dot(t)
+        }
+
+        points[2] = points[1].add(t.multiply(h));
+
+        // calculate fourth point to make it appear as a rotated bounding box
+        const fourthPoint = points[2].add(points[0].subtract(points[1]));
+
+        super(new paper.Path({
+            segments: [points[0], points[1], points[2], fourthPoint],
+            closed: true,
+            strokeColor: "#00ff00",
+        }))
+    }
+
+    updateCorner(i, e) {
+        const shape = this.children[0]
+
+        if (e.modifiers.control) {
+            // if the control key is pressed rotate the shape around the opposite corner
+            const center = shape.segments[(i + 2) % 4].point
+            const angle = e.point.subtract(center).angle - shape.segments[i].point.subtract(center).angle
+            shape.rotate(angle, center)
+        } else {
+            // otherwise keep the angle fixed and adjust the sides of the rectangle
+            const opposite = shape.segments[(i + 2) % 4]
+
+            const s1 = shape.segments[(i + 1) % 4].point.subtract(opposite.point).normalize()
+            const s2 = shape.segments[(i + 3) % 4].point.subtract(opposite.point).normalize()
+
+            // project the vector from the opposite corner to the current corner onto the sides of the rectangle
+            const d = e.point.subtract(opposite.point)
+            const d1 = d.dot(s1)
+            const d2 = d.dot(s2)
+
+            shape.segments[i].point = e.point
+            shape.segments[(i + 1) % 4].point = opposite.point.add(s1.multiply(d1))
+            shape.segments[(i + 3) % 4].point = opposite.point.add(s2.multiply(d2))
+        }
+
+        this.updateHandles()
+    }
+
+    get points() {
+        return [this.children[0].segments[0].point, this.children[0].segments[1].point, this.children[0].segments[2].point]
+    }
+}
+
+class LabelPolygon extends Label {
+    static min_points = 2
+    static num_points = -1
+    constructor(points) {
+        super(new paper.Path({
+            segments: points,
+            closed: true,
+            strokeColor: "#00ff00",
+        }))
+    }
+
+    updateCorner(i, e) {
+        const shape = this.children[0]
+
+        shape.segments[i].point = e.point
+
+        this.updateHandles()
+    }
+
+    get points() {
+        return this.children[0].segments.map(s => s.point)
+    }
+}
+
+
 onMounted(() => {
     paper.setup(canvas.value)
+    paper.settings.handleSize = 10
     view.value = paper.view
+    project.value = paper.project
 })
 
 onUnmounted(() => {
@@ -69,7 +231,7 @@ function onResize() {
     view.value.viewSize = new paper.Size(rect.width, rect.height)
 
     if (sceneImage.value === null)
-        return;
+        return
 
     const prevImageSize = sceneImage.value.bounds.size;
     const prevImagePos = sceneImage.value.position;
@@ -84,7 +246,7 @@ function onResize() {
 
     // apply the scale and translation to the shapes
     for (let i = 0; i < sceneShapes.value.length; i++) {
-        const shape = sceneShapes.value[i][1];
+        const shape = sceneShapes.value[i];
 
         shape.scale(scale, prevImagePos);
         shape.translate(translation);
@@ -92,12 +254,20 @@ function onResize() {
 }
 
 function onClick(e) {
+    // check if there is already a shape being drawn
+    if (project.value.selectedItems.length > 0 && !currentShape.value)
+        return;
+
+    // check if the selected tool is valid
+    if (!selectedTool.value)
+        return;
+
     // add the point to the buffer points
     bufferPoints.push(e.point);
 
     // check if the buffer points are enough to draw the shape
-    if (bufferPoints.length === shapeLengths[selectedTool.value]) {
-        currentShape.value.selected = false;
+    if (currentShape.value && bufferPoints.length === currentShape.value.constructor.num_points) {
+        currentShape.value.unfocus()
 
         // normalize the coordinates wrt the scene image
         for (let i = 0; i < bufferPoints.length; i++) {
@@ -105,7 +275,7 @@ function onClick(e) {
         }
 
         // add the shape to the scene shapes
-        sceneShapes.value.push([selectedTool.value, currentShape.value, bufferPoints]);
+        sceneShapes.value.push(currentShape.value);
 
         // clear
         bufferPoints = [];
@@ -133,12 +303,17 @@ function onMouseWheel(e) {
     // Trackpad pinch-zoom
     const scale = Math.exp(-e.deltaY / 1000);
 
+    // Dont allow zooming out if the image is too small
+    if (sceneImage.value.bounds.width < 100 || sceneImage.value.bounds.height < 100 && scale < 1) {
+        return;
+    }
+
     const origin = view.value.viewToProject(paper.DomEvent.getOffset(e, canvas.value));
 
     sceneImage.value.scale(scale, origin);
 
     for (let i = 0; i < sceneShapes.value.length; i++) {
-        const shape = sceneShapes.value[i][1];
+        const shape = sceneShapes.value[i];
         shape.scale(scale, origin);
     }
 
@@ -220,7 +395,7 @@ function loadSample(id) {
         for (const l of labels[files[id].name] || []) {
             const points = l.points.map(p => p.multiply(sceneImage.value.bounds.size).add(sceneImage.value.bounds.topLeft));
             const shape = drawShape(l.type, points);
-            sceneShapes.value.push([l.type, shape, l.points]);
+            sceneShapes.value.push(shape);
         }
 
         // Update the sample id
@@ -233,78 +408,31 @@ function saveCurrentSample() {
         return;
     }
 
-    labels[files[currentSampleId.value].name] = sceneShapes.value.map(([type, shape, points]) => {
+    labels[files[currentSampleId.value].name] = sceneShapes.value.map((shape) => {
         return {
-            type: type,
-            points: points,
+            type: shape.constructor.name,
+            points: shape.points.map(p => p.subtract(sceneImage.value.bounds.topLeft).divide(sceneImage.value.bounds.size)),
         };
     });
 }
 
 // draw logic
 function drawShape(type, points, preview = false) {
-    switch (type) {
-        case "newBoundingBox":
-            return drawBoundingBox(points, preview);
-        case "newRotatedBoundingBox":
-            return drawRotatedBoundingBox(points, preview);
-        case "newPolygon":
-            return drawPolygon(points, preview);
-        default:
-            return null;
-    }
-}
+    const shapeClass = {
+        "LabelRectangle": LabelRectangle,
+        "LabelRotatedRectangle": LabelRotatedRectangle,
+        "LabelPolygon": LabelPolygon
+    }[type];
 
-function drawBoundingBox(points, preview) {
-    if (points.length < 2) {
+    if (points.length < shapeClass.min_points)
         return;
-    }
 
-    return new paper.Path.Rectangle({
-        from: points[0],
-        to: points[1],
-        strokeColor: "#00ff00",
-        selected: preview,
-    });
-}
+    const shape = new shapeClass(points)
 
-function drawRotatedBoundingBox(points, preview) {
-    if (points.length < 2) {
-        return;
-    }
+    if (preview)
+        shape.focus()
 
-    let s = points[1].subtract(points[0]).normalize();
-    let t = new paper.Point(-s.y, s.x).normalize();
-
-    let h = 0
-    if (points.length === 3) {
-        h = points[2].subtract(points[1]).dot(t)
-    }
-
-    points[2] = points[1].add(t.multiply(h));
-
-    // calculate fourth point to make it appear as a rotated bounding box
-    const fourthPoint = points[2].add(points[0].subtract(points[1]));
-
-    return new paper.Path({
-        segments: [points[0], points[1], points[2], fourthPoint],
-        closed: true,
-        strokeColor: "#00ff00",
-        selected: preview,
-    });
-}
-
-function drawPolygon(points, preview) {
-    if (points.length < 2) {
-        return;
-    }
-
-    return new paper.Path({
-        segments: points,
-        closed: true,
-        strokeColor: "#00ff00",
-        selected: preview,
-    });
+    return shape
 }
 
 // download logic
@@ -341,6 +469,7 @@ function downloadAnnotationsYolo(labels) {
 }
 
 function downloadAnnotations() {
+    saveCurrentSample();
     downloadAnnotationsYolo(labels);
 }
 
@@ -396,12 +525,12 @@ div#__next {
 
 <template>
     <div class="menu-bar" v-if="state !== 'idle'">
-        <div class="menu-item" :class="{ selected: selectedTool === 'newBoundingBox' }"
-            @click="selectedTool = (selectedTool === 'newBoundingBox') ? '' : 'newBoundingBox'">
+        <div class="menu-item" :class="{ selected: selectedTool === 'LabelRectangle' }"
+            @click="selectedTool = (selectedTool === 'LabelRectangle') ? '' : 'LabelRectangle'">
             <img src="/icons/bounding-box.svg" alt="New Bounding Box" />
         </div>
-        <div class="menu-item" :class="{ selected: selectedTool === 'newRotatedBoundingBox' }"
-            @click="selectedTool = (selectedTool === 'newRotatedBoundingBox') ? '' : 'newRotatedBoundingBox'">
+        <div class="menu-item" :class="{ selected: selectedTool === 'LabelRotatedRectangle' }"
+            @click="selectedTool = (selectedTool === 'LabelRotatedRectangle') ? '' : 'LabelRotatedRectangle'">
             <img src="/icons/rotated-bounding-box.svg" alt="New Rotated Bounding Box" />
         </div>
         <div class="menu-item" @click="downloadAnnotations">
